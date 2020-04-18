971c9d38b0756daadb594ae2b3ac0729f2ca8bb6
Lugovskoi <58656757+Lugovskoi@users.noreply.github.com>
2020-04-18 16:37:02 +0300

    create base Dao and UserDao

--- database/src/main/java/by/clowns/Main.java
+++ database/src/main/java/by/clowns/Main.java
@@ -1,24 +1,19 @@
 package by.clowns;
 
+import by.clowns.dao.Dao;
+import by.clowns.dao.UserDao;
 import by.clowns.entity.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        final Configuration configuration = new Configuration().configure();
-        final SessionFactory sessionFactory = configuration.buildSessionFactory();
-        final Session session = sessionFactory.openSession();
-        final Transaction transaction = session.beginTransaction();
 
-        User user = session.get(User.class, 1L);
-        Car car = session.get(Car.class, 1L);
-        System.out.println(user);
-        transaction.commit();
+        Dao<User> dao = UserDao.getInstance();
 
-        session.close();
-        sessionFactory.close();
+        Set<User> users = dao.reed();
+
+        System.out.println(users.iterator().hasNext());
+
     }
 }
---
+++ database/src/main/java/by/clowns/dao/Dao.java
@@ -0,0 +1,16 @@
+package by.clowns.dao;
+
+import java.util.Set;
+
+public interface Dao <T> {
+
+    void create(T entity);
+
+    Set<T> reed();
+
+    void update(T entity, long id);
+
+    void delete(long id);
+
+    T get(long id);
+}
