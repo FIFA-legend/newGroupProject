ec1ecb5d0ef2c0ed933c632a514c69e9840ef2d4
Lugovskoi <58656757+Lugovskoi@users.noreply.github.com>
2020-04-18 23:04:39 +0300

    Create CarDao and connect Car with Region

--- database/src/main/java/by/clowns/Main.java
+++ database/src/main/java/by/clowns/Main.java
@@ -1,25 +1,25 @@
 package by.clowns;
 
+import by.clowns.dao.CarDao;
 import by.clowns.dao.Dao;
 import by.clowns.dao.RegionDao;
 import by.clowns.dao.UserDao;
 import by.clowns.entity.*;
 
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        Dao<Region> dao = RegionDao.getInstance();
+        Dao<Car> carDao = CarDao.getInstance();
+        Dao<Region> regionDao = RegionDao.getInstance();
 
-        Region region = new Region();
-        region.setName("Voshod");
+        Car car = carDao.get(1);
 
-        dao.create(region);
-//        Set<Region> users = dao.reed();
-
-//        System.out.println(users.iterator().hasNext());
-        System.out.println(region);
+        carDao.close();
+        regionDao.close();
+        System.out.println(car);
     }
 }
