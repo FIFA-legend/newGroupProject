2decca741a73e0691da8f6581d78d652c4d71417
Lugovskoi <58656757+Lugovskoi@users.noreply.github.com>
2020-06-17 00:32:49 +0300

    Начало реализации фильтра

---
+++ web/src/main/java/by/clowns/config/ThymeleafConfig.java
@@ -0,0 +1,50 @@
+package by.clowns.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class ThymeleafConfig {
+
+    @Value("${web.character.encoding}")
+    private String characterEncoding;
+
+    @Value("${web.prefix}")
+    private String prefix;
+
+    @Value("${web.suffix}")
+    private String suffix;
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding(characterEncoding);
+        templateResolver.setPrefix(prefix);
+        templateResolver.setSuffix(suffix);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding(characterEncoding);
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+}
--- web/src/main/java/by/clowns/config/WebConfiguration.java
+++ web/src/main/java/by/clowns/config/WebConfiguration.java
@@ -1,10 +1,7 @@
 package by.clowns.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -14,41 +11,7 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "by.clowns.controllers")
-@PropertySource("classpath:config.properties")
+@Import(value = {ThymeleafConfig.class})
 public class WebConfiguration {
-
-    @Value("${web.character.encoding}")
-    private String characterEncoding;
-
-    @Value("${web.prefix}")
-    private String prefix;
-
-    @Value("${web.suffix}")
-    private String suffix;
-
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setCharacterEncoding(characterEncoding);
-        templateResolver.setPrefix(prefix);
-        templateResolver.setSuffix(suffix);
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setEnableSpringELCompiler(true);
-        engine.setTemplateResolver(templateResolver());
-        return engine;
-    }
-
-    @Bean
-    public ViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setCharacterEncoding(characterEncoding);
-        viewResolver.setTemplateEngine(templateEngine());
-        return viewResolver;
-    }
 
 }
